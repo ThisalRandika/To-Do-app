5207479d8ef97108206641a29d0fa8bce28dd6d5
ThisalRandika <161827081+ThisalRandika@users.noreply.github.com>
2025-02-19 12:07:57 +0530

    Add files via upload

---
+++ to-do list app/script.js
@@ -0,0 +1,39 @@
+const inputBox = document.getElementById("input-box");
+const listContent = document.getElementById("list-content");
+
+function addTask(){
+    if(inputBox.value ===''){
+        alert("You must write something!");
+    }
+    else{
+        let li =document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContent.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML= "\u00d7";
+        li.appendChild(span);
+
+
+    }
+    inputBox.value ="";
+    saveData();
+}
+listContent.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data",listContent.innerHTML);
+}
+
+function showTask(){
+    listContent.innerHTML = localStorage.getItem("data");
+}
+showTask();
